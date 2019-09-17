c77f71db62aa913ea07c6427beca3502e4e01af4
Nazim Boudeffa <boudeffa@gmail.com>
2019-09-17 17:05:26 +0200

    Added basic database operations integrated with expressjs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var mysql = require('mysql');
-pass = require('../config-chaudoudoux.js');
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+var pass = require('../config-chaudoudoux.js');
 
 var con = mysql.createConnection({
   host: "164.132.103.232",
@@ -12,3 +15,39 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+app.get('/', function (req, res) {
+  res.send("Hello Chaudoudoux! The database state is " + con.state + " ...");
+})
+
+app.get('/insert', function (req, res) {
+  var sql = "INSERT INTO cdd_users (id, first_name, last_name, address, zip_code, city) VALUES ('1234', 'Jaques', 'Chabrol', '42, Rue Jean Jaurès', '35700', 'Rennes')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    //console.log("1 record inserted");
+    res.send("1 record inserted");
+  });
+})
+
+app.get('/list', function (req, res) {
+  con.query("SELECT * FROM cdd_users", function (err, result, fields) {
+    if (err) throw err;
+    //console.log(result);
+    res.send(result);
+  });
+})
+
+app.get('/delete', function (req, res) {
+  var sql = "DELETE FROM cdd_users WHERE first_name = 'Jaques'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    //console.log("Number of records deleted: " + result.affectedRows);
+    res.send("Number of records deleted: " + result.affectedRows);
+  });
+})
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!')
+})
