d5ba0d810ef0dc21ae7faab949fdd90c8d0430a6
Nazim Boudeffa <boudeffa@gmail.com>
2019-09-19 18:23:23 +0200

    Added a login page and moved the db con to the admin one

--- app.js
+++ app.js
@@ -3,67 +3,82 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 
 const app = express();
-var pass = require('../config-chaudoudoux.js');
-
-var con = mysql.createConnection({
-  host: "164.132.103.232",
-  user: "chaudoudoux",
-  password: pass,
-  database: "chaudoudoux"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+//var pass = require('../config-chaudoudoux.js');
+var con;
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded())
 
 app.get('/', function (req, res) {
-  res.send("Welcome to Chaudoudoux!");
+
+  res.render('index');
+
 })
 
 app.get('/admin', function (req, res) {
-  res.render('index');
+
+  res.render('admin');
+
 })
 
 app.post('/admin/login', function (req,res) {
+
   var username = 'chaudoudoux';
   var pass = req.body.password;
   console.log(pass);
-  res.end("Hello " + username + "! The database state is " + con.state + " ...");
+
+  con = mysql.createConnection({
+    host: "164.132.103.232",
+    user: "chaudoudoux",
+    password: pass,
+    database: "chaudoudoux"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    res.send("Hello " + username + "! The database state is " + con.state + " ...");
+  });
+
 });
 
 app.get('/admin/insert', function (req, res) {
+
   var sql = "INSERT INTO cdd_users (id, first_name, last_name, address, zip_code, city) VALUES ('1234', 'Jaques', 'Chabrol', '42, Rue Jean Jaurès', '35700', 'Rennes')";
   con.query(sql, function (err, result) {
     if (err) throw err;
     //console.log("1 record inserted");
     res.send("1 record inserted");
   });
+
 })
 
 app.get('/admin/list', function (req, res) {
+
   con.query("SELECT * FROM cdd_users", function (err, result, fields) {
     if (err) throw err;
     //console.log(result);
     res.send(result);
   });
+
 })
 
 app.get('/admin/delete', function (req, res) {
+
   var sql = "DELETE FROM cdd_users WHERE first_name = 'Jaques'";
   con.query(sql, function (err, result) {
     if (err) throw err;
     //console.log("Number of records deleted: " + result.affectedRows);
     res.send("Number of records deleted: " + result.affectedRows);
   });
+
 })
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
+
   console.log('Example app listening on port 3000!')
+  
 })
