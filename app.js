ad8a9c621b4edb7b77d4a43da3f892809e9fd79a
Nazim Boudeffa <boudeffa@gmail.com>
2019-09-25 17:39:19 +0200

    Added passportjs with a local strategy and related pages like the profile

--- app.js
+++ app.js
@@ -2,17 +2,101 @@ const express = require('express');
 const mysql = require('mysql');
 const ejs = require('ejs');
 const passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./db');
 const CryptoJS = require('crypto-js');
 
 const app = express();
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
 //var pass = require('../config-chaudoudoux.js');
 var con;
 
-app.set('view engine', 'ejs');
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      console.log("ok " + username + " " + user.password);
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != CryptoJS.MD5(password)) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
 
-app.use(require('body-parser').urlencoded({ extended: true }));
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 app.get('/', function (req, res) {
+
+  res.render('home', { user: req.user });
+
+})
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+app.get('/signup', function (req, res) {
+
+  res.render('signup');
+
+})
+
+/**
+ This is just for testing purposes, we'll use passportjs instead
+*/
+app.get('/signin', function (req, res) {
 
   con = mysql.createConnection({
     host: "164.132.103.232",
@@ -21,13 +105,7 @@ app.get('/', function (req, res) {
     database: "chaudoudoux"
   });
 
-  res.render('index');
-
-})
-
-app.get('/signup', function (req, res) {
-
-  res.render('signup');
+  res.render('signin');
 
 })
 
@@ -55,6 +133,8 @@ app.post('/signin', function (req, res) {
   });
 
 })
+// END TEST
+
 
 app.get('/admin', function (req, res) {
 
---
+++ db/interveners.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql');
+
+var con;
+var records;
+
+con = mysql.createConnection({
+  host: "164.132.103.232",
+  user: "unicorn",
+  password: "IAM_ateapot101",
+  database: "chaudoudoux"
+});
+
+con.query("SELECT * FROM cdd_interveners", function (err, result, fields) {
+  records = result;
+});
+
+exports.findById = function(id, cb) {
+  process.nextTick(function() {
+    console.log(records);
+    var idx = id - 1;
+    if (records[idx]) {
+      cb(null, records[idx]);
+    } else {
+      cb(new Error('User ' + id + ' does not exist'));
+    }
+  });
+}
+
+exports.findByUsername = function(username, cb) {
+  process.nextTick(function() {
+    for (var i = 0, len = records.length; i < len; i++) {
+      var record = records[i];
+      if (record.username === username) {
+        return cb(null, record);
+      }
+    }
+    return cb(null, null);
+  });
+}
