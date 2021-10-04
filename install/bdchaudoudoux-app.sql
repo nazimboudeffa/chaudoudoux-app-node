3a5186a84a7c729f0a9a1662adef5513c645a84c
nazimboudeffa <boudeffa@gmail.com>
2021-10-04 18:56:21 +0200

    DB

--- install/bdchaudoudoux-app.sql
+++ install/bdchaudoudoux-app.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : lun. 04 oct. 2021 à 17:14
--- Version du serveur :  10.4.17-MariaDB
--- Version de PHP : 8.0.1
+-- Hôte : localhost:3306
+-- Généré le : lun. 04 oct. 2021 à 16:55
+-- Version du serveur :  8.0.26-0ubuntu0.20.04.3
+-- Version de PHP : 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,8 +29,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cdd_my_tasks` (
-  `id_user` int(11) NOT NULL,
-  `id_task` int(11) NOT NULL
+  `id_user` int NOT NULL,
+  `id_task` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -39,7 +40,7 @@ CREATE TABLE `cdd_my_tasks` (
 --
 
 CREATE TABLE `cdd_profile` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
@@ -54,7 +55,7 @@ CREATE TABLE `cdd_profile` (
 --
 
 CREATE TABLE `cdd_tasks` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -65,7 +66,7 @@ CREATE TABLE `cdd_tasks` (
 --
 
 CREATE TABLE `cdd_users` (
-  `id` int(11) NOT NULL,
+  `id` bigint NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
